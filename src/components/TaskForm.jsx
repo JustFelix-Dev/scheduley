dc5584f9e05777f0ec2a0e90fea0c2ec6978c09a
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-07 09:02:56 +0100

    scheduley

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -94,7 +94,7 @@ const TaskForm = ({ mode = 'edit', task }) => {
              <>
                 <h3 className='taskText'>{task?.name}</h3>
                 <div className="title">
-                      <motion.form initial={{opacity:0,x:-50,y:50}} animate={{opacity:1,x:0,y:0}} transition={{duration:1}} onSubmit={handleSubmit}>
+                      <motion.form initial={{opacity:0}} animate={{opacity:1}}  onSubmit={handleSubmit}>
                        <div className='anchor'></div>
                     <h2>{mode === 'create' ? 'Create a Task' : 'Edit a Task'}</h2>
                         <label htmlFor="taskName">Task Name:</label> <br />
